757efae544fb31ec73c3d4ae26c060b20740aded
Rachel Padworski <rachelpadworski@gmail.com>
2020-04-05 00:35:45 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,11 @@
 require_relative '../lib/concerns/memorable'
 
 module Memorable
-  def self.reset_all
+  def reset_all
     self.all.clear
   end
 
-  def self.count
+  def count
     self.all.count
   end
 end
